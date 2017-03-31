84e9c7fed353d7ba63a256564a0f8768297e869c
orearow <oreoluwa.arowobusoye@ucalgary.ca>
2017-03-31 11:41:19 -0600

    new sql table

---
+++ superfan(brandons changes).sql
@@ -0,0 +1,20 @@
+﻿# Host: localhost  (Version 5.7.17-log)
+# Date: 2017-03-31 11:36:18
+# Generator: MySQL-Front 6.0  (Build 1.86)
+
+
+#
+# Structure for table "movie"
+#
+
+DROP TABLE IF EXISTS `movie`;
+CREATE TABLE `movie` (
+  `release` int(11) NOT NULL,
+  `plot synopsis` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "movie"
+#
+
